db57e14a4ebd2631163810b821f145f3b35d5741
Kyle Joyce <kyyjoyce@gmail.com>
2017-02-23 15:34:22 -0700

    tutorial complete

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,8 @@ import { combineReducers } from 'redux';
 import BooksReducer from './books';
 import ActiveBook from './active-book';
 
+// all reducers are combined in this function
+// each property in this function will have it's own reducers
 const rootReducer = combineReducers({
   books: BooksReducer,
   activeBook: ActiveBook
